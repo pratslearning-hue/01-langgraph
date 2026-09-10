18812d569f34ad74502fefc745b646cd6b0b6abd
prathamesh-12 <53848995+mansiHaldankar@users.noreply.github.com>
2026-09-10 09:45:12 +0530

    initial setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
+from langchain_core import agents
+
+load_dotenv()
+
+
+def main():
+    print("Hello from 01-lang-graph-course!")
+
+
+if __name__ == "__main__":
+    main()
