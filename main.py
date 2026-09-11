adf3ee5657887e9767e22e8032c5b89376197476
prathamesh-12 <53848995+mansiHaldankar@users.noreply.github.com>
2026-09-11 09:59:28 +0530

    nodes added and plumbed into main function

--- main.py
+++ main.py
@@ -3,12 +3,44 @@ import os
 from dotenv import load_dotenv
 
 from langchain_core import agents
+from langchain_core.messages import HumanMessage
+from langgraph.graph import StateGraph, MessagesState, END
+
+from node import run_agent_reasoning, tool_node
 
 load_dotenv()
+
+AGENT_REASON = "agent_reason"
+ACT = "act"
+LAST = -1
+
+def should_continue(state: MessagesState) -> MessagesState:
+    if not state["messages"][LAST].tool_calls:
+        return END
+    return ACT
+
+flow = StateGraph(MessagesState)
+
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+
+flow.add_conditional_edges(AGENT_REASON, should_continue, {
+    END: END,
+    ACT: ACT
+})
+
+flow.add_edge(ACT, AGENT_REASON)
+
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
+
 
 
 def main():
     print("Hello from 01-lang-graph-course!")
+    res = app.invoke({"messages": [HumanMessage(content="Whats the weather in Pune? List it and triple it")]})
+    print(res["messages"][LAST].content)
 
 
 if __name__ == "__main__":
